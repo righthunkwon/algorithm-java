fb0e970b573c749d352752a872389e47ee9d93fe
kwonjeonghun <righthun.kwon@gmail.com>
2023-01-11 21:00:03 +0900

    2023.1.11

---
+++ CodeUp100/src/CodeUp1021.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CodeUp1021 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		System.out.println(a);
+	}
+
+}
---
+++ CodeUp100/src/CodeUp1022.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CodeUp1022 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String a = scan.nextLine();
+		System.out.print(a);
+	}
+
+}
+
+	/*
+			next() 메서드와 nextLine() 메서드의 차이
+			
+				next()		: Space, 즉 공백 전까지 입력받은 문자열을 리턴
+				nextLine()	: Enter를 치기 전까지 쓴 문자열을 모두 리턴
+	*/	
