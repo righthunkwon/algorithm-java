6fb66d82b2c17f2b0cbd6ed8b468f40594bf7f3c
hippoDev <righthun.kwon@gmail.com>
2023-03-01 23:59:13 +0900

    2023.3.1 Baekjoon lv3 solve

---
+++ Baekjoon/src/level_3/P_10951.java
@@ -0,0 +1,22 @@
+package level_3;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class P_10951 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while(true) {
+			try {
+				int A = scan.nextInt();
+				int B = scan.nextInt();
+				System.out.println(A+B);
+			} catch (NoSuchElementException nsee) {
+				scan.close();
+				break;
+			}
+		}
+	}
+
+}
---
+++ Baekjoon/src/level_3/P_2439.java
@@ -0,0 +1,23 @@
+package level_3;
+
+import java.util.Scanner;
+
+public class P_2439 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		scan.close();
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
