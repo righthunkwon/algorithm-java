fbd1f7efb16c73884f8b94bb72424db907d0c1b1
hippoDev <righthun.kwon@gmail.com>
2023-03-03 19:28:03 +0900

    2023.3.3 Baekjoon lv 4 arrays solve

---
+++ Baekjoon/src/level_4/P_10818.java
@@ -0,0 +1,28 @@
+package level_4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P_10818{
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int index = 0;
+		int[] arr = new int[N];
+		
+		while(st.hasMoreElements()) {
+			arr[index] = Integer.parseInt(st.nextToken());
+			index++;
+		}
+		
+		Arrays.sort(arr);
+		System.out.print(arr[0] + " " + arr[N-1]);
+	}
+}
