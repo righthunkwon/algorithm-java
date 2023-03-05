7599f5cba1a14fda2ad1527eb357f2a31625fafe
hippoDev <righthun.kwon@gmail.com>
2023-03-05 15:42:27 +0900

    2023.3.5 Baekjoon lv4 array solve

--- Baekjoon/src/level_4/P_5597.java
+++ Baekjoon/src/level_4/P_5597.java
@@ -6,14 +6,14 @@ public class P_5597 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int[] arr = new int[30];
+		int[] arr = new int[31];
 		
-		for(int i=0; i<30; i++) {
+		for(int i=1; i<29; i++) {
 			int present = scan.nextInt();
 			arr[present] = 1;
 		}
 		
-		for(int i=0; i<arr.length; i++) {
+		for(int i=1; i<arr.length; i++) {
 			if(arr[i] != 1) {
 				System.out.println(i);
 			}
