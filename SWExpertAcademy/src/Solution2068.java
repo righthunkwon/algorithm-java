8628f4b733f0d7a7f6c60008824abb4c70e68bd0
hippoDev <righthun.kwon@gmail.com>
2023-07-04 20:45:48 +0900

    2023.07.04 SWEA 2068

--- SWExpertAcademy/src/Solution2068.java
+++ SWExpertAcademy/src/Solution2068.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
 
 public class Solution2068 {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 1; i <= T; i++) {
+			int max = 0;
+			for(int j = 0; j < 10; j++) {
+				int num = sc.nextInt();
+				if(num > max) {
+					max = num;
+				}
+			}
+			System.out.println("#" + (i+1) + " " + max);
+			max = 0;
+		}
 	}
 
 }
