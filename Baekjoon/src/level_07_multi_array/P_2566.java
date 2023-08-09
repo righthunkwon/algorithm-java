8aeda0b3d0ce9e9b2c4d6aaf5041725af8f5aa8e
hippoDev <righthun.kwon@gmail.com>
2023-08-09 23:27:26 +0900

    boj solve

---
+++ Baekjoon/src/level_07_multi_array/P_2566.java
@@ -0,0 +1,26 @@
+package level_07_multi_array;
+
+import java.util.Scanner;
+
+public class P_2566 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[9][9];
+		int max = 0;
+		int maxi = 0;
+		int maxj = 0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				arr[i][j] = sc.nextInt();
+				if (max < arr[i][j]) {
+					max = arr[i][j];
+					maxi = i;
+					maxj = j;
+				}
+			}
+		}
+		System.out.println(max);
+		System.out.println((maxi + 1) + " " + (maxj + 1));
+
+	}
+}
