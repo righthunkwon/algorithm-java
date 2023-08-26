4e19b23a6ca5bd48462dfad3bc3013aa84db0c19
hippoDev <righthun.kwon@gmail.com>
2023-08-26 20:57:59 +0900

    boj class 1 finished

---
+++ Baekjoon/src/level_00_not_classified/P_10250.java
@@ -0,0 +1,30 @@
+package level_00_not_classified;
+
+import java.util.Scanner;
+
+public class P_10250 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+
+		for (int t = 1; t <= tc; t++) {
+			int h = sc.nextInt(); // 높이
+			int w = sc.nextInt(); // 높이별 호수
+			int n = sc.nextInt(); // 손님의 순서
+
+			int height = n % h; // 높이
+			int roomnumber = n / h + 1; // 방번호
+			
+			// 손님의 순서와 높이가 동일할 경우(n = h)
+			if (height == 0) {
+				height = h;
+				roomnumber = n / h;
+			}
+			
+			// 출력
+			// String.format("%02d", number)
+			// %: 명령의 시작, 0: 채워질 문자, 2: 총 자리수, d: 십진정수
+			System.out.println(height + String.format("%02d", roomnumber));
+		}
+	}
+}
---
+++ Baekjoon/src/level_00_not_classified/P_8958.java
@@ -0,0 +1,27 @@
+package level_00_not_classified;
+
+import java.util.Scanner;
+
+public class P_8958 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tc = sc.nextInt();
+		for (int t = 1; t <= tc; t++) {
+			String str = sc.next();
+			char[] ch = str.toCharArray();
+			int ans = 0; // 정답
+			int cnt = 1; // 변화하는 점수
+			for (int i = 0; i < ch.length; i++) {
+				if(ch[i] == 'O') {
+					ans += cnt;
+					if (i < ch.length - 1 && ch[i+1] == 'O') {
+						cnt++;
+					}
+				} else {
+					cnt = 1;
+				}
+			}
+			System.out.println(ans);
+		}
+	}
+}
