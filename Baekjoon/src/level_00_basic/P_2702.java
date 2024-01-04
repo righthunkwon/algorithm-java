06b1bfd7ace0c6e5b9193df45f7d4bd472d66660
hippoDev <righthun.kwon@gmail.com>
2024-01-05 08:59:20 +0900

    boj solve

---
+++ Baekjoon/src/level_00_basic/P_2702.java
@@ -0,0 +1,20 @@
+package level_00_basic;
+
+import java.util.Scanner;
+
+// 초6 수학
+// 유클리드 호재법 적용해보기
+public class P_2702 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int LCM = 0;
+			int GCD = 0;
+		}
+		
+		sc.close();
+	}
+}
