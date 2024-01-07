5ad637bbc26323cc779936a1b1ca55bf8b94afc6
hippoDev <righthun.kwon@gmail.com>
2024-01-07 09:20:46 +0900

    boj solve

--- Baekjoon/src/level_00_basic/P_2702.java
+++ Baekjoon/src/level_00_basic/P_2702.java
@@ -3,18 +3,32 @@ package level_00_basic;
 import java.util.Scanner;
 
 // 초6 수학
-// 유클리드 호재법 적용해보기
 public class P_2702 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
+		int a, b;
 		for (int tc = 1; tc <= T; tc++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int LCM = 0;
-			int GCD = 0;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			int GCD = 0, LCM = 0;;
+			
+			// 최대공약수
+			for (int i = 1000; i >= 1; i--) {
+				if (a % i == 0 && b % i == 0) {
+					GCD = i;
+					break;
+				}
+			}
+			// 최소공배수
+			for (int i = 1; i <= a*b; i++) {
+				if (i % a == 0 && i % b == 0) {
+					LCM = i;
+					break;
+				}
+			}
+			System.out.println(LCM + " " + GCD);
 		}
-		
 		sc.close();
 	}
 }
