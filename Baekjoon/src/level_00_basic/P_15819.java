bc4af5f7fd9cb7d2db24b5a6aae22a40ff134def
권정훈 <righthun.kwon@gmail.com>
2024-01-08 23:22:50 +0900

    boj solve

--- Baekjoon/src/level_00_basic/P_15819.java
+++ Baekjoon/src/level_00_basic/P_15819.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.util.*;
 
 // 너의 핸들은
+// 문자보다 숫자가 우선순위
 public class P_15819 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,8 +13,9 @@ public class P_15819 {
 		st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
 		int L = Integer.parseInt(st.nextToken());
+		
 		String[] sarr = new String[N+1];
-		sarr[0] = "";
+		sarr[0] = ""; // 초기화
 		for (int i = 1; i <= N; i++) sarr[i] = br.readLine();
 		Arrays.sort(sarr);
 		System.out.println(sarr[L]);
