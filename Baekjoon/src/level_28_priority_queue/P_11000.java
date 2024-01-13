94dab29d8be1d031947c621049827ff6055ccf2b
권정훈 <righthun.kwon@gmail.com>
2024-01-13 13:23:20 +0900

    boj solve

---
+++ Baekjoon/src/level_28_priority_queue/P_11000.java
@@ -0,0 +1,12 @@
+package level_28_priority_queue;
+
+import java.io.*;
+import java.util.*;
+
+// 강의실 배정
+public class P_11000 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+}
